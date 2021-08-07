b2fe4fda3a7a469f8530f327936e6cab0e9bdc48
Jessica Joseph <jessicaajosephh@gmail.com>
2021-08-07 19:03:48 -0400

    set up the constructor for the recipe class so that all the attributes are set to the instance of this and used ES6 syntax for the comments so that it spreads them

--- models/Recipe.js
+++ models/Recipe.js
@@ -1,3 +1,11 @@
 class Recipe {
-    
+    constructor(title, ingredients, prep_time, cook_time, directions, likes, comments){
+        this.title = title
+        this.ingredients = ingredients 
+        this.prep_time = prep_time
+        this.cook_time = cook_time 
+        this.directions = directions 
+        this.likes = likes
+        this.comment = [...comments]
+    }
 }
