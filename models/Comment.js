5eec3cba23101b697ef78b67604ce559e06891fc
Jessica Joseph <jessicaajosephh@gmail.com>
2021-08-07 19:08:07 -0400

    set up the constructor for the comment class so that all the attributes are set to the instance of this

--- models/Comment.js
+++ models/Comment.js
@@ -1,3 +1,9 @@
 class Comment {
-    
+
+    constructor(id, content, likes, recipe_id){
+        this.id = id 
+        this.content = content
+        this.likes = likes 
+        this.recipe_id = recipe_id 
+    }
 }
