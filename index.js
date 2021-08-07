d62b655b6e2281ac3c58886510a9f7df27b54902
Jessica Joseph <jessicaajosephh@gmail.com>
2021-08-07 19:31:55 -0400

    added code that is now commented out that I will be getting back to after

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+// let titleInput = () => document.getElementById("title")
+// let ingredientsInput = () => document.getElementById("ingredients")
+// let cook_timeInput = () => document.getElementById("cook_time")
+// let prep_timeInput = () => document.getElementById("prep_time")
+// let directionsInput = () => document.getElementById("directions")
+
 const startProgram = () => {
 
 }
