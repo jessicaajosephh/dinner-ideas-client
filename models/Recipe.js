17343a3548a5031dcde09ca62cc6cd9f98e2dfdc
Jessica Joseph <jessicaajosephh@gmail.com>
2021-08-07 20:15:22 -0400

    added a majority of the code for renderRecipes

--- models/Recipe.js
+++ models/Recipe.js
@@ -19,7 +19,21 @@ class Recipe {
 
     static renderRecipes(recipeInfo){
         recipeInfo.forEach(recipe => {
-            
+            let div = document.createEelement("div")
+            let h2 = document.createElement("h2")
+            let p = document.createElement("p")
+            let h4 = document.createElement("h4")
+            let h4 = document.createElement("h4")
+            let p = document.createElement("p")
+            let likeButton = document.createElement("button")
+
+            div.id = recipe.id 
+            h2.innerText = recipe.title 
+            p.innerText = recipe.ingredients 
+            h4.innerText = recipe.prep_time 
+            h4.innerText = recipe.cook_time 
+            p.innerText = recipe.directions
+            likeButton.innerText = "♥"
         })
     }
 
